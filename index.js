cd6884320103b20e5bb22175b1cdc2cfaa85f6ac
Manny <mhfernan35277@gmail.com>
2023-08-06 18:51:23 -0700

    fetch-lab

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
+  return fetch("https://anapioficeandfire.com/api/books")
+  .then(function (res) {
+    return res.json();
+  })
+  .then(function (json) {
+    console.log(renderBooks(json))
+  })
   
 }
 
